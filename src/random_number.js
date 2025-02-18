672cf9792553528dbc0361cb9f23e750ca113a88
EphemeralSock <146490686+EphemeralSock@users.noreply.github.com>
2025-02-19 00:33:20 +0300

    Update random_number.js

--- src/random_number.js
+++ src/random_number.js
@@ -1,15 +1,19 @@
-const getRandomNumber = (num1, num2) => {
-    if (arguments.length <= 1) { 
-      const range = num1; 
-      if (range === undefined) {
-        return 0;
-      }
-      return Math.floor(Math.random() * range); 
-    } else { 
-      const min = Math.min(num1, num2);
-      const max = Math.max(num1, num2);
-      return min + Math.floor(Math.random() * (max - min)); 
+const getRandomNumber = (...args) => {
+  if (args.length === 0) {
+    return 0; 
+  } else if (args.length === 1) { 
+    const range = args[0]; 
+    if (range === undefined) {
+      return 0; 
     }
-  };
+    return Math.floor(Math.random() * range); /
+  } else { 
+    const num1 = args[0];
+    const num2 = args[1];
+    const min = Math.min(num1, num2);
+    const max = Math.max(num1, num2);
+    return min + Math.floor(Math.random() * (max - min)); 
+  }
+};
 
-  export default getRandomNumber;
+export default getRandomNumber;
