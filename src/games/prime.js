e753b46384279086c3aa00d9e5949565114ec088
EphemeralSock <146490686+EphemeralSock@users.noreply.github.com>
2025-02-25 17:53:23 +0300

    Update prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -4,13 +4,13 @@ import getRandomNumber from '../random_number.js'
 const isPrime = (num) => {
     if (num < 2) {
         return false;
-      }
-    for (let i = 2; i < num; i += 1){
-        if (num % i ===0) {
-            return false;
+    }
+    for (let i = 2; i < num; i += 1) {
+        if (num % i === 0) {
+            return false; 
         }
-            return true;
-    };
+    }
+    return true; 
 };
 
 const primeAnswer = () => {
