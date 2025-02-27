a7403aead6b31237506cbba666db0311364c6148
EphemeralSock <146490686+EphemeralSock@users.noreply.github.com>
2025-02-27 19:50:08 +0300

    Update even.js

--- src/games/even.js
+++ src/games/even.js
@@ -1,17 +1,17 @@
-import runGame from  '../index.js'
-import getRandomNumber from '../random_number.js'
+import runGame from '../index.js';
+import getRandomNumber from '../random_number.js';
 
+const isEven = (num) => num % 2 === 0;
 
-const getKindOfNumber = (num) => {
+const getKindOfNumber = () => {
     const maxNum = getRandomNumber(1000);
     const question = `${maxNum}`;
-    const isEven = (num) => num % 2 === 0;
     const result = (isEven(maxNum) ? 'yes' : 'no');
     return [question, result];
-    };
+};
 
 const brainEven = () => {
-    const gameTask = 'Answer "yes" if the number is even, otherwise answer "no"'
+    const gameTask = 'Answer "yes" if the number is even, otherwise answer "no"';
     runGame(gameTask, getKindOfNumber);
 };
 
