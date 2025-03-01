814131962ddcb530c2ebb7e4adcda7c0b9b833a4
EphemeralSock <146490686+EphemeralSock@users.noreply.github.com>
2025-03-01 23:31:09 +0300

    Update gcd.js

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,12 +1,16 @@
 import runGame from '../index.js';
 import getRandomNumber from '../random_number.js';
 
-const getGcd = (num1, num2) => {
+const getGcd = (initialNum1, initialNum2) => {
+  let num1 = initialNum1;
+  let num2 = initialNum2;
+
   while (num2) {
     const temp = num2;
     num2 = num1 % num2;
     num1 = temp;
   }
+  
   return num1.toString();
 };
 
